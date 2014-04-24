1a86a501497bbb36d526824251dc3551fc92177c
Insomniac10102 <ins10102@gmail.com>
2014-04-24 02:23:45 -0700

    Initial start on settings page. Deleted scripts.
    Now that most of the buttons exist and are in place, the actual
    functionality can be implemented.

--- src/index.php
+++ src/index.php
@@ -24,26 +24,8 @@
 		<link rel="stylesheet" type="text/css" href="toolbartheme.css">
 		<link rel="canonical" href="http://www.relatablez.com/">
 		
-		<script type="text/javascript">
-		  (function() {
-			var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-			po.src = 'https://apis.google.com/js/plusone.js?onload=onLoadCallback';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-		  })();
-		</script>
-		<script>(function(d, s, id) {
-		  var js, fjs = d.getElementsByTagName(s)[0];
-		  if (d.getElementById(id)) return;
-		  js = d.createElement(s); js.id = id;
-		  js.async = true;
-		  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-		  fjs.parentNode.insertBefore(js, fjs);
-		  }(document, 'script', 'facebook-jssdk'));
-		</script>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.async=true;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 	</head>
 	<body>		
-		<div id="fb-root"></div>
 		<?php require($_SERVER["DOCUMENT_ROOT"] . "/toolbar.php"); ?>
 		
 		<div id='main'>
