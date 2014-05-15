6d1e5d98d6c04c72e77e7162c1a57d9f57a0014e
Insomniac10102 <ins10102@gmail.com>
2014-05-14 23:11:24 -0700

    Added errors.php*
    *The errors need to be polished. There are placeholders for you (Adam)
    to rephrase them however you see fit. These will be the messages we spit
    out to the user next to the "x" on an input field.

---
+++ src/errors.php
@@ -0,0 +1,40 @@
+<?php
+	/*Copyright (C) Tyler Hackett 2014*/
+	
+	////////////////////////////////////
+	//
+	//	This is a list of premade error messages to send to the user, in the event that they
+	//	did something wrong (or stupid in some cases). All error messages should be user/idiot-
+	//	friendly, as they'll be used to send directly to the user in certain parts of the site.
+	//
+	////////////////////////////////////
+	
+	$_USR_ERR = 
+	array(
+		"Username too long.",
+		"Username too short.",
+		"Username contains invalid characters.",
+	);
+	
+	$_PSW_ERR =
+	array(
+		"Password too short.",
+		"Re entered password does not match"
+	);
+	
+	$_EML_ERR =
+	array(
+		"Email too long.",
+		"Email too short.",
+		"Invalid email." // This would be triggered if the provided email does not contain both @ sign and a period.
+	);
+	
+	$_CTR_ERR =
+	array(
+		"Invalid country code." //Every country has an id. There are 250 countries in our database. any id less than 1 and greater than 250 is invalid.
+	);
+	
+	$_DSC_ERR = 
+	array(
+		"Description too long."
+	);
