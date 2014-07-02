023cd70f1aecce921f170c9e114ae006b04d9e8b
Insomniac10102 <ins10102@gmail.com>
2014-07-02 04:20:57 -0700

    Smaller verifyEmail.php overhead
    This was meant to be committed in the last one, stupid github.

--- src/verifyEmail.php
+++ src/verifyEmail.php
@@ -1,16 +1,16 @@
 <?php
 	/*Copyright (C) Tyler Hackett 2014*/
 	
-	if(!isset($_POST["email"]))
-		die("email not set");
+	if(!isset($_POST['e']))
+		die('2');
 		
-	$email = $_POST["email"];
+	$email = $_POST['e'];
 	
-	$connection = mysqli_connect("mysql.a78.org","u683362690_insom","10102S33K3R17","u683362690_rtblz");
+	$connection = mysqli_connect('mysql.a78.org','u683362690_insom','10102S33K3R17','u683362690_rtblz');
 	
-	if($statement = $connection->prepare("SELECT 1 FROM accounts WHERE email LIKE (?)"))
+	if($statement = $connection->prepare('SELECT 1 FROM accounts WHERE email LIKE (?)'))
 	{
-		$statement->bind_param("s",$email);
+		$statement->bind_param('s',$email);
 		
 		$statement->execute();
 		
@@ -18,15 +18,15 @@
 		
 		if(empty($result))
 		{
-			die("email available");
+			die('0');
 		}
 		else
 		{
-			die("email unavailable");
+			die('1');
 		}
 	}
 	else
 		echo $connection->error;
 		
-	echo "somehow, nothing happened";
+	echo 'somehow, nothing happened';
 ?>
