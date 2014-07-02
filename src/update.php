3c01b73876297382b12980b9451c39dcd964611f
Insomniac10102 <ins10102@gmail.com>
2014-07-02 03:51:28 -0700

    Fixed backend username verification

--- src/update.php
+++ src/update.php
@@ -11,33 +11,38 @@
 	{
 	
 		$user = $_POST['username'];
+		$userLen = strLen($user);
 		
 		if(($user !== null) && ($user !== ''))
-		{		
-				
-			if(!preg_match('/^[A-Za-z0-9_]+$/',$user)) // Check that username only contains alphanumerics and underscore at most
-				header('Location: http://www.relatablez.com/settings/account?e=0&i=2');
-				
-			if((!(strcasecmp($user,$_SESSION['username']) == 0)) && ($user !== $_SESSION['username']))
+		{
+			if(($userLen >= 3) && ($userLen <= 16))
 			{
-				$connection = mysqli_connect('mysql.a78.org','u683362690_insom','10102S33K3R17','u683362690_rtblz');
-
-				if($statement = $connection->prepare('SELECT username FROM accounts WHERE username LIKE (?)'))
+				if(preg_match('/^[A-Za-z0-9_]+$/',$user)) // Check that username only contains alphanumerics and underscore at most
 				{
-					$statement->bind_param('s',$user);
+					if((!(strcasecmp($user,$_SESSION['username']) == 0)) && ($user !== $_SESSION['username']))
+					{
+						$connection = mysqli_connect('mysql.a78.org','u683362690_insom','10102S33K3R17','u683362690_rtblz');
+
+						if($statement = $connection->prepare('SELECT username FROM accounts WHERE username LIKE (?)'))
+						{
+							$statement->bind_param('s',$user);
+							
+							$statement->execute();
+							
+							$statement->bind_result($dbUser);
+							$result = $statement->fetch();
+							
+							if(empty($result))
+							{		
+								setUsername($user,$_SESSION['id']);
+								$_SESSION['username'] = $user;
+							}
+						}
+					}
 					
-					$statement->execute();
 					
-					$statement->bind_result($dbUser);
-					$result = $statement->fetch();
-					
-					if(!empty($result))
-						header('Location: http://www.relatablez.com/settings/account');
 				}
 			}
-			
-			setUsername($user,$_SESSION['id']);
-			$_SESSION['username'] = $user;
 		}
 		
 		$old_pass = $_POST['oldpassword'];
