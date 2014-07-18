c6205cea4b27a04ba1014e47bd0636faed452d8d
Insomniac10102 <ins10102@gmail.com>
2014-07-18 01:10:17 -0700

    Started moderate page

--- src/moderate/index.php
+++ src/moderate/index.php
@@ -1,42 +1,33 @@
 <?php  
-	$connection = mysqli_connect("mysql25.freehostia.com","tylhac_aitoo","10102S33K3R17","tylhac_aitoo");
+	$connection = mysqli_connect("mysql.a78.org","u683362690_insom","10102S33K3R17","u683362690_rtblz");
 
 	if (mysqli_connect_errno())
 	{
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}	
 	
-	$submissions = mysqli_query($connection,"SELECT * FROM submissions WHERE pending = 1 ORDER BY submissions.date DESC LIMIT 0, 20");
-
+	$submissions = mysqli_query($connection,"SELECT submission, id FROM submissions WHERE pending = 1 ORDER BY submissions.date DESC LIMIT 0, 20");
+	
+	$submission = mysqli_fetch_array($submissions);
 ?>
 <html>
 	<head>
 		<title>Am I The Only One?</title>
 		<link rel="shortcut icon" href="favicon.ico"/>
-		<link rel="stylesheet" type="text/css" href="../indextheme.css">
+		<link rel='stylesheet' type='text/css' href='moderate.css'>
+		<link rel="stylesheet" type="text/css" href="/toolbartheme.css">
 	</head>
 	<body>
 		<?php require("../toolbar.php"); ?>
 		<div id='main'>
-			<div style='float:left;width:600px;'>
-				<?php
-					while($row = mysqli_fetch_array($submissions))
-					{
-						echo 
-						"<div class='dialogue' id='" . $row["id"] . "'>
-							<p class='dialogue'>" . $row["submission"] . "</p>
-							<button class='dialoguebutton' onclick='vote(na" . $row["id"] . ")'><label id='na" . $row["id"] . "'>" . $row["notalone"] . "</label> - I do this, too!</button>
-							<button class='dialoguebutton' onclick='vote(a" . $row["id"] . ")'><label id='a" . $row["id"] . "'>" . $row["alone"] . "</label> - You're alone.</button>
-							<div style='text-align:right;'>
-								<label>Submitted by: " . $row["username"] . " on " . $row["date"] . "</label>
-							</div>
-						</div>";
-					}
-				?>	
-			</div>		
-			
-			<?php include("../adcontent.html"); ?>
-			
+			<div style='width:100%;background:#BFBFBF;'>
+				<div class='pending-submission-wrapper' id='<?php echo $submission["id"]; ?>'>
+					<h2>Does this post obey the rules & guidelines?</h2>
+					<p class='pending-submission'><?php echo $submission["submission"]; ?></p>
+					<button class='vote green' >Yes</button>
+					<button class='vote red' >No</button>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
