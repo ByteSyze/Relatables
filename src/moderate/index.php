4906b9d230e790dcbd668b6665e59d5c6b7cbff0
Insomniac10102 <ins10102@gmail.com>
2014-07-23 14:00:31 -0700

    Added vote increment system

--- src/moderate/index.php
+++ src/moderate/index.php
@@ -1,12 +1,17 @@
 <?php  
-	$connection = mysqli_connect("mysql.a78.org","u683362690_insom","10102S33K3R17","u683362690_rtblz");
+	$connection = mysqli_connect('mysql.a78.org','u683362690_insom','10102S33K3R17','u683362690_rtblz');
 
 	if (mysqli_connect_errno())
 	{
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
 	}	
 	
-	$submissions = mysqli_query($connection,"SELECT submission, id FROM submissions WHERE pending = 1 ORDER BY submissions.date DESC LIMIT 0, 20");
+	$index = $_POST['i'];
+	
+	if($index == null)
+		$index = 0;
+	
+	$submissions = mysqli_query($connection,'SELECT submission, id FROM submissions WHERE pending = 1 ORDER BY submissions.date DESC LIMIT ' . $index . ', ' . ++$index);
 	
 	$submission = mysqli_fetch_array($submissions);
 ?>
@@ -21,14 +26,24 @@
 		<?php require("../toolbar.php"); ?>
 		<div id='main'>
 			<div style='width:100%;background:#C4C4C4;'>
-				<div class='pending-submission-wrapper' id='<?php echo $submission["id"]; ?>'>
+				<div class='pending-submission-wrapper' >
 					<h2>Does this post obey the rules & guidelines?</h2>
-					<p class='pending-submission'><?php echo $submission["submission"]; ?></p>
-					<form action=''>
-						<button class='vote green' >Yes</button>
-						<button class='vote red' >No</button>
+					<p class='pending-submission'>
+						<?php 
+							if($submission['submission'] !== null)
+								echo $submission['submission'];
+							else
+								echo '<b>There are no more posts to moderate!</b>';
+						?>
+					</p>
+					<form method='POST'>
+						<input type='submit' name='v' class='vote green' value='Yes' />
+						<input type='submit' name='v' class='vote red' value='No' />
+						<input type='hidden' name='pid' value='<?php echo $submission['id']; ?>' />
+						<input type='hidden' name='i' value='<?php echo $index; ?>' />
 					</form>
 				</div>
+				<a id='help'>Help</a>
 			</div>
 			<div id='guidelines'>
 				<span class='guideline-title'><b>Guidelines:</b></span>
