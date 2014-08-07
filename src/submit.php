3a2993b834c7747546cd8fd8632eed5525204d4c
Insomniac10102 <ins10102@gmail.com>
2014-08-07 02:27:50 -0700

    Fixed bug pertaining to old submission structure.

--- src/submit.php
+++ src/submit.php
@@ -9,12 +9,14 @@
 	{	
 		$connection = mysqli_connect("mysql.a78.org","u683362690_insom","10102S33K3R17","u683362690_rtblz");
 		
-		if($statement = $connection->prepare("INSERT INTO submissions (uid, category, submission, anonymous) VALUES (?,?,?,?)"))
+		if($statement = $connection->prepare("INSERT INTO submissions (uid, verification, category, submission, anonymous) VALUES (?,?,?,?,?)"))
 		{	
-			$statement->bind_param("iis",$_SESSION['id'], $category, $submission, $anon);
+			$statement->bind_param("iiisi",$_SESSION['id'], openssl_random_pseudo_bytes(4), $category, $submission, $anon);
 			
 			$statement->execute();
 		}
+		else
+			die($connection->error);
 	}
 	
 	header('Location: http://www.relatablez.com/');
