5f7486152132b126833b71d71c1ab08d614d3563
Insomniac10102 <ins10102@gmail.com>
2014-08-22 04:02:41 -0700

    Fixed up nsfw functionality, renamed vars

--- src/getsubmissions.php
+++ src/getsubmissions.php
@@ -1,9 +1,9 @@
 <?php
 	
 	//Convert a numerical code to MYSQL syntax for ordering a query.
-	function order2mysql($num)
+	function order2mysql($order)
 	{
-		switch($num)
+		switch($order)
 		{
 			case 0:
 				return 'ORDER BY submissions.date ASC';
@@ -19,24 +19,33 @@
 	}
 	
 	//Convert a numerical code to MYSQL syntax for selecting a submission category.
-	function cat2mysql($num)
+	function cat2mysql($cat)
 	{
-		if($num >= 0 && $num <= 100)
-			return 'AND category = '.$num;
+		if($cat >= 0 && $cat <= 100)
+			return 'AND category = '.$cat;
 		else
 			return '';
 	}
 	
+	//Convert a numerical code to MYSQL syntax for including NSFW posts.
+	function nsfw2mysql($nsfw)
+	{
+		if($nsfw == true)
+			return '';
+		else
+			return 'AND nsfw=0';
+	}
+	
 	$start		= $_GET['s'] ? $_GET['s'] : 0;
 	$count 		= $_GET['x'] ? $_GET['x'] : 20;
-	$nsfw		= $_GET['n'] ? $_GET['n'] : 0;
 	
 	$order 		= order2mysql($_GET['o']);
 	$category 	= cat2mysql($_GET['c']);
+	$nsfw		= nsfw2mysql$_GET['n']);
 	
 	$connection = mysqli_connect('mysql.a78.org','u683362690_insom','10102S33K3R17','u683362690_rtblz');
 	
-	if($statement = $connection->prepare("SELECT *, DATE_FORMAT(date,'%M %d, %Y') AS fdate FROM submissions WHERE pending = 0 AND nsfw=(?) $order DESC LIMIT (?), (?)"))
+	if($statement = $connection->prepare("SELECT *, DATE_FORMAT(date,'%M %d, %Y') AS fdate FROM submissions WHERE pending = 0 $nsfw $category $order DESC LIMIT (?), (?)"))
 	{
 		$statement->bind_param('iii', $nsfw, $start, $start+$count);
 		$statement->execute();
