ed062b619f544870db5acb313b61ced4d17319b1
Insomniac10102 <ins10102@gmail.com>
2014-09-13 02:30:59 -0700

    QOTW stuff
    Alot of work was done to the mysql databases, so not much to show here.

--- src/comment.php
+++ src/comment.php
@@ -5,6 +5,15 @@
 		die('1');
 	
 	$comment = $_POST['c'];
+	$clen = strlen($comment);
 	
-	if(strlen($comment))
+	if($clen < 1 || $clen > 140)
 		die('2');
+	
+	$connection = mysqli_connect('mysql.a78.org','u683362690_insom','10102S33K3R17','u683362690_rtblz');
+	
+	if($statement = $connection->prepare("INSERT INTO comments (pid, comment, uid, rid, reply) VALUES (?,?,?,?,?)"))
+	{
+		
+	}
+		
--- src/qotw.php
+++ src/qotw.php
@@ -13,6 +13,19 @@
 	$yes_prcnt = (int)($yes_votes/$total_votes*100);
 	$no_prcnt  = (int)($no_votes/$total_votes*100);
 	
+	$connection = mysqli_connect('mysql.a78.org','u683362690_insom','10102S33K3R17','u683362690_rtblz');
+	
+	$query = $connection->query("SELECT id FROM qotw ORDER BY created LIMIT 1");
+	$qotw = $query->fetch_row();
+	
+	if($statement = $connection->prepare("REPLACE INTO qotw_votes (uid, v, qid) VALUES ({$_SESSION['id']}, ?, {$qotw['id']})"))
+	{
+		$statement->bind_param('i',$vote);
+		$statement->execute();
+	}
+	
+	$query = $connection->query("SELECT option, id FROM qotw_options WHERE qid = {$qotw['id']}");
+	
 	if($yes_votes > $no_votes)
 		die("<b>Yes ($yes_votes) ($yes_prcnt%)</b><br><span>No ($no_votes) ($no_prcnt%)</span>");
 	else
