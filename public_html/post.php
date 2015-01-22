25e995d5b62f0436c39ec4d24870b8d7772000c1
Insomniac10102 <ins10102@gmail.com>
2015-01-21 18:14:51 -0800

    More work on Post class

--- public_html/post.php
+++ public_html/post.php
@@ -9,6 +9,7 @@
 		private $verification;	/**4-byte verification code associated with this post.*/
 		private $category; 		/**numerical representation of this post's category.*/
 		private $fdate; 		/**Formatted date this post was submitted on.*/
+		private $date_diff;		/**Time (in minutes) since this post was created.*/
 		private $alone; 		/**Number of "alone" votes.*/
 		private $notalone; 		/**Number of "not alone" votes.*/
 		private $pending;		/**Whether or not this post is pending approval.*/
@@ -25,26 +26,40 @@
 		 * */
 		public function __construct()
 		{
-			$connection = GlobalUtils::getConnection();
+			$this->connection = GlobalUtils::getConnection();
 			
 			if(func_num_args()>0)//If an anything is passed in, treat it as a post ID
 			{
 				$id = func_get_arg(0);
-				if($statement = $connection->prepare("SELECT (SELECT username FROM accounts where id=uid), verification, category, DATE_FORMAT(date,'%M %d, %Y') AS date, alone, notalone, pending, submission, anonymous, (SELECT admin FROM accounts WHERE id=submissions.uid), (SELECT COUNT(cid) FROM comments WHERE pid=(?) AND rid=0) FROM submissions WHERE id=(?)"))
+				if($statement = $this->connection->prepare("SELECT (SELECT username FROM accounts where id=uid), verification, category, DATE_FORMAT(date,'%M %d, %Y'), (UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(date))/60, alone, notalone, pending, submission, anonymous, (SELECT admin FROM accounts WHERE id=submissions.uid), (SELECT COUNT(cid) FROM comments WHERE pid=(?) AND rid=0) FROM submissions WHERE id=(?)"))
 				{
 					$statement->bind_param('ii', $id, $id);
 					$statement->execute();
 					
-					$statement->bind_result($this->username,$this->verification,$this->category,$this->fdate,$this->alone,$this->notalone,$this->pending,$this->submission,$this->anonymous,$this->admin,$this->comment_count);
+					$statement->bind_result($this->username,$this->verification,$this->category,$this->fdate,$this->date_diff,$this->alone,$this->notalone,$this->pending,$this->submission,$this->anonymous,$this->admin,$this->comment_count);
 					$statement->fetch();
 				}
 			}
 		}
 		
+		private function calculateDateDifference()
+		{
+			if($this->date_diff/60/24/365 >= 1)
+				$date_diff = '(' . floor($this->date_diff/60/24/365) . ' years ago)'; 
+			else if($this->date_diff/60/24 >= 1)
+				$date_diff = '(' . floor($this->date_diff/60/24) . ' days ago)'; 
+			else if($this->date_diff/60 >= 1)
+				$date_diff = '(' . floor($this->date_diff/60) . ' hours ago)'; 
+			else
+				$date_diff = '(' . floor($this->date_diff) . ' minutes ago)';
+				
+			return $date_diff;
+		}
+		
 		/**Prints a formatted AITOO post.*/
 		public function format()
 		{
-			echo "\r\n<div class='dialogue uppadding' id='{$this->id}' data-category='{$this->category}' data-nsfw='{$this->nsfw}' data-date='{$this->fdate}'>";
+			echo "\r\n<div class='dialogue uppadding' id='{$this->id}' data-category='{$this->category}' data-nsfw='{$this->nsfw}' data-date='{$this->date_diff}'>";
 			echo "\r\n<p class='dialogue'>{$this->submission}</p>";
 			echo "\r\n<table class='vote-table'>";
 			echo "\r\n<tr>";
@@ -74,12 +89,12 @@
 			echo "\r\n<div style='text-align:right;'><span class='submissioninfo'><a ";
 			
 			if($this->anonymous)
-				echo " >Anonymous</a> - $this->fdate</span></div>";
+				echo " >Anonymous</a> - " . $this->calculateDateDifference() . "</span></div>";
 			else
 			{
 				if($this->admin)
 					echo 'class=\'admin\'';
-				echo " href='http://www.relatablez.com/user/$this->user'>$this->user</a> - $this->fdate</span></div>";
+				echo " href='http://www.relatablez.com/user/" . $this->user . "'>" . $this->user</a> . " - " . $this->calculateDateDifference() . "</span></div>";
 			}
 			echo "\r\n</div>";
 		}
