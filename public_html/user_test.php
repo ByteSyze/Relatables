7f1cdd954d6a4028fefedd6a69279172066ce64f
Insomniac10102 <ins10102@gmail.com>
2015-02-04 18:28:09 -0800

    Testing user class

--- public_html/user_test.php
+++ public_html/user_test.php
@@ -9,6 +9,9 @@
 		echo ' Mere peasants cannot see this message. <span style="color:red;">Muahahaha!</span> ';
 		
 	$user->setUsername('Adam Sander');
+	$user->setAdmin();
+	
+	$user->update();
 	
 	echo 'Your name has been changed to "' . $user->getUsername() . '"';
 	
