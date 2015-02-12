4fb0c361ce010bfbe882c869e07c2f8bce9d4b78
seanbarker <contact@seanb.me>
2015-02-11 20:06:34 -0500

    Some work on comments

--- public_html/getcomments.php
+++ public_html/getcomments.php
@@ -35,52 +35,60 @@
 		if($user == $_SESSION['username'] && !$deleted)
 			echo "<button data-delete class='delete'></button>";
 		
-		echo "<a href='http://www.relatablez.com/user/$user'>$user</a>";
+		echo "<div class='comment-info'>";
+			echo "<span><a class='user' href='/user/$user'>$user</a></span>";
 			
-		if($points < 0)
-			echo "<span id='points-$cid' class='points negative'>$points</span>";
-		else if($points > 0)
-			echo "<span id='points-$cid' class='points positive'>$points</span>";
-		else
-			echo "<span id='points-$cid' class='points'>$points</span>";
+			$points_class = "";
+			if($points < 0) $points_class = "negative";
+			else if($points > 0) $points_class = "positive";
+
+			echo "<span id='points-$cid' class='points $points_class'>$points</span>";
+				
+			$submitted = DateTime::createFromFormat("m d Y H i", $submitted);
+			$time_diff = $submitted->diff($now);
 			
-		$submitted = DateTime::createFromFormat("m d Y H i", $submitted);
-		$time_diff = $submitted->diff($now);
-		
-		if($time_diff->y)
-			$time_diff = $time_diff->y . ' years ago';
-		else if($time_diff->m)
-			$time_diff = $time_diff->m . ' months ago';
-		else if($time_diff->d)
-			$time_diff = $time_diff->d . ' days ago';
-		else if($time_diff->h)
-			$time_diff = $time_diff->h . ' hours ago';
-		else
-			$time_diff = $time_diff->i . ' minutes ago';
-		
-		echo "<span>$time_diff</span>";
-		
-		if($deleted)
-		{
-			echo "<p><i>Comment removed by author.</i></p>";
-		}
-		else if($reported > 0)
-		{
-			echo "<p><i>Comment removed by an administrator.</i></p>";
-		}
-		else if($rid != 0)
-		{
-			$rUserPos = strpos($comment, ' ');
-			$rUser = substr($comment, 0, $rUserPos);
-			$comment = substr($comment, $rUserPos, strlen($comment));
-			echo "<p><a href='http://www.relatablez.com/user/$rUser'>@$rUser</a> $comment</p>";
-		}
-		else
-			echo "<p>$comment</p>";
-		
-		echo "<span data-reply>Reply</span><button data-v='up' class='up vote'></button><button data-v='down' class='down vote'></button><span data-report>Report</span>";
+			if($time_diff->y)
+				$time_diff = $time_diff->y . ' years ago';
+			else if($time_diff->m)
+				$time_diff = $time_diff->m . ' months ago';
+			else if($time_diff->d)
+				$time_diff = $time_diff->d . ' days ago';
+			else if($time_diff->h)
+				$time_diff = $time_diff->h . ' hours ago';
+			else
+				$time_diff = $time_diff->i . ' minutes ago';
 			
-		echo "</div>\r\n";
+			echo "<span>$time_diff</span>";
+		echo "</div>";
+		
+		echo "<div class='comment-body'>";
+			if($deleted)
+			{
+				echo "<span class='muted'>Comment removed by author.</span>";
+			}
+			else if($reported > 0)
+			{
+				echo "<span class='muted'>Comment removed by an administrator.</span>";
+			}
+			else if($rid != 0)
+			{
+				$rUserPos = strpos($comment, ' ');
+				$rUser = substr($comment, 0, $rUserPos);
+				$comment = substr($comment, $rUserPos, strlen($comment));
+				echo "<p><a href='http://www.relatablez.com/user/$rUser'>@$rUser</a> $comment</p>";
+			}
+			else
+				echo "$comment";
+		echo "</div>";
+		
+		echo "<div class='buttons'>";
+			echo "<span data-reply class='button smaller blue-hover'>Reply</span>";
+			echo "<button data-v='up' class='button smaller green-hover'>&#11014;</button>";
+			echo "<button data-v='down' class='button smaller red-hover'>&#11015;</button>";
+			echo "<span data-report class='button smaller red-hover'>Report</span>";
+		echo "</div>";
+
+		echo "</div>";
 	}
 	
 	$num_rows = $statement->num_rows;
