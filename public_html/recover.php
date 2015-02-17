4f4743ec2ae08498e8d643630b50f68a58696b41
Insomniac10102 <ins10102@gmail.com>
2015-02-16 21:38:07 -0800

    Work on the second half

--- public_html/recover.php
+++ public_html/recover.php
@@ -3,14 +3,15 @@
 	
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/global.php';
 	
-	$email 	= $_POST['e'];
+	$email 	= $_GET['e']; //TODO change back to POST once testing is over.
+	
 	$verification = $_GET['v'];
 	
 	$connection = GlobalUtils::getConnection();
 	
 	if($email)   //If an email is provided, send a verification link to it.
 	{
-		if($statement = $connection->prepare('SELECT verification, id WHERE email LIKE (?)'))
+		if($statement = $connection->prepare('SELECT verification, id FROM accounts WHERE email LIKE (?)'))
 		{
 			$statement->bind_param('s', $email);
 			$statement->execute();
@@ -27,6 +28,23 @@
 	}
 	else if($verification) //If verification is provided, attempt to verify the password recovery and send user to the password reset page.
 	{
-		
+		if($statement = $connection->prepare('SELECT id FROM accounts WHERE verification=(?)'))
+		{
+			$statement->bind_param('s', $verification);
+			$statement->execute();
+			
+			$statement->bind_result($uid);
+			
+			if(!$uid)
+			{
+				$_SESSION['popup_msg'] = 'Invalid verification.'; //TODO do something with this
+				header('Location: http://www.relatablez.com/');
+			}
+			else
+			{
+				$_SESSION['recover_uid'] = $uid;
+				header('Location: http://www.relatablez.com/recover/');
+			}
+		}
 	}
 	
