a7eca80e2e01cd34eb05f687b87bb54bd6292c2c
SeanBarker <contact@seanb.me>
2015-02-18 14:26:32 -0500

    Work on faq, privacy, terms, etc

---
+++ public_html/about/aboutnav.php
@@ -0,0 +1,7 @@
+
+      <nav id='navigation'>
+        <a href='http://www.relatablez.com/blog/'>Blog</a>
+        <a href='http://www.relatablez.com/FAQ/'>FAQ</a>
+        <a href='http://www.relatablez.com/privacy/' class='selected'>Privacy</a>
+        <a href='http://www.relatablez.com/terms/'>Terms</a>
+      </nav>
