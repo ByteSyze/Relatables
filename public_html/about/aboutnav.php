6d4c65a5a715c82943aaeacf86ceb265016b7634
SeanBarker <contact@seanb.me>
2015-02-18 22:35:53 -0500

    add about to the sidebar navigation

--- public_html/about/aboutnav.php
+++ public_html/about/aboutnav.php
@@ -3,6 +3,7 @@
     <div class="box">
       <div class="box-content">
         <div class="about-nav">
+          <a href="/about">About</a>
           <a href='/about/blog'>Blog</a>
           <a href='/about/faq'>FAQ</a>
           <a href='/about/privacy' class='selected'>Privacy</a>
