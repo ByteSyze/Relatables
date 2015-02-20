4ad8fb47a79abe9f41d8763446545fb9635dacb2
SeanBarker <contact@seanb.me>
2015-02-20 16:05:20 -0500

    Remove general settings page

--- public_html/settings/settingsnav.php
+++ public_html/settings/settingsnav.php
@@ -3,7 +3,6 @@
     <div class="box">
       <div class="box-content">
         <div class="settings-nav">
-          <a href="/settings/">General</a>
           <a href='/settings/account'>Account</a>
           <a href='/settings/profile'>Profile</a>
         </div>
