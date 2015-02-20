7ce4ea2cd44dd5d40ff4c043225e7d397d95ddf5
SeanBarker <contact@seanb.me>
2015-02-20 14:58:26 -0500

    Start work on settings pages

---
+++ public_html/settings/settingsnav.php
@@ -0,0 +1,13 @@
+<div class="unit one-fourth">
+  <div class="boxes">
+    <div class="box">
+      <div class="box-content">
+        <div class="settings-nav">
+          <a href="/settings/">General</a>
+          <a href='/settings/account'>Account</a>
+          <a href='/settings/profile'>Profile</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
