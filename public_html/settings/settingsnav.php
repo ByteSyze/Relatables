0950db7be7c963c06a5caa846eba1c2566df8175
Insomniac10102 <ins10102@gmail.com>
2015-03-04 13:49:57 -0800

    Add settings redirect page, admin link on settings navigation

--- public_html/settings/settingsnav.php
+++ public_html/settings/settingsnav.php
@@ -5,6 +5,7 @@
         <div class="settings-nav">
           <a href='/settings/account'>Account</a>
           <a href='/settings/profile'>Profile</a>
+		  <?php if(GlobalUtils::$user->isAdmin()) echo "\r\n<a href='/settings/admin'>Admin</a>";?>
         </div>
       </div>
     </div>
