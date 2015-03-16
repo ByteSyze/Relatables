49ef8d2d2da69ec9cb3537f5624e77e5f0692e9e
Insomniac10102 <ins10102@gmail.com>
2015-03-16 14:43:01 -0700

    Generate verification code in recover.php

--- public_html/recover.php
+++ public_html/recover.php
@@ -2,6 +2,7 @@
 	/*Copyright (C) Tyler Hackett 2015*/
 	
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/global.php';
+	require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/password.php';
 	
 	$email 	= $_GET['e']; //TODO change back to POST once testing is over.
 	
@@ -11,19 +12,26 @@
 	
 	if($email)   //If an email is provided, send a verification link to it.
 	{
-		if($statement = $connection->prepare('SELECT verification, id FROM accounts WHERE email LIKE (?)'))
+		if($statement = $connection->prepare('SELECT id FROM accounts WHERE email LIKE (?)'))
 		{
 			$statement->bind_param('s', $email);
 			$statement->execute();
 			
-			$statement->bind_result($verification, $uid);
+			$statement->bind_result($uid);
 			$statement->fetch();
 			
 			if($uid) //Assure the user exists
 			{
 				$user = new User($uid);
 				
+				$verification 		= md5(openssl_random_pseudo_bytes(256, $crypto_strong));
+				$verification_hash 	= password_hash($verification, PASSWORD_DEFAULT);
+				
 				$body = "Hey " . $user->getUsername() . ",\n\nYou are receiving this email because a request for password recovery has been made.\n\nPlease goto the link below to reset your password.\nhttp://www.relatablez.com/recover.php?v=$verification \n\nIf you didn't request password recovery, ignore this message.\n\nThanks,\nThe Relatablez Team";
+				
+				$user->setVerification($verification_hash);
+				$user->update();
+				
 				$user->email('Password Recovery', $body);
 			}
 		}
