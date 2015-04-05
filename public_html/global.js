d0f5b9cfd54aa77b0d31b32b5cd0acbab633d919
Insomniac10102 <ins10102@gmail.com>
2015-04-05 02:28:23 -0700

    Comment out currently irrelevant JS, fix vote button selectors

--- public_html/global.js
+++ public_html/global.js
@@ -378,12 +378,12 @@ function createPopup(message)
 
 function vote(id, vote, v)
 {
-	var notAloneEl  = document.getElementById('na'+id);
-	var aloneEl 	= document.getElementById('a'+id);
+	//var notAloneEl  = document.getElementById('na'+id);
+	//var aloneEl 	= document.getElementById('a'+id);
 	
 	// Take out all formatting
-	var notAlone = notAloneEl.innerHTML.replace("(","").replace(")","").replace(",",""); 
-	var alone = aloneEl.innerHTML.replace("(","").replace(")","").replace(",","");
+	//var notAlone = notAloneEl.innerHTML.replace("(","").replace(")","").replace(",",""); 
+	//var alone = aloneEl.innerHTML.replace("(","").replace(")","").replace(",","");
 	
 	$.ajax({
 		type: "POST",
@@ -395,37 +395,37 @@ function vote(id, vote, v)
 		
 		if(data == '00')
 		{
-			alone++;
-			notAlone--;
+			//alone++;
+			//notAlone--;
 			
-			document.getElementById('bna'+id).disabled = false;
-			document.getElementById('ba'+id).disabled = true;
+			$('button.green-hover[data-vid="'+id+'"]').disabled = false;
+			$('button.red-hover[data-vid="'+id+'"]').disabled = true;
 			
-			notAloneEl.innerHTML = '(' + notAlone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
-			aloneEl.innerHTML = '(' + alone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
+			//notAloneEl.innerHTML = '(' + notAlone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
+			//aloneEl.innerHTML = '(' + alone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
 		}
 		else if(data == '01')
 		{	
-			notAlone++;
-			alone--;
+			//notAlone++;
+			//alone--;
 			
-			document.getElementById('bna'+id).disabled = true;
-			document.getElementById('ba'+id).disabled = false;
+			$('button.green-hover[data-vid="'+id+'"]').disabled = true;
+			$('button.red-hover[data-vid="'+id+'"]').disabled = false;
 			
 			notAloneEl.innerHTML = '(' + notAlone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
 			aloneEl.innerHTML = '(' + alone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
 		}
 		else if(data == '10')
 		{
-			alone++;
-			document.getElementById('ba'+id).disabled = true;
-			aloneEl.innerHTML = '(' + alone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
+			//alone++;
+			$('button.red-hover[data-vid="'+id+'"]').disabled = true;
+			//aloneEl.innerHTML = '(' + alone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
 		}
 		else if(data == '11')
 		{
-			notAlone++;
-			document.getElementById('bna'+id).disabled = true;
-			notAloneEl.innerHTML = '(' + notAlone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
+			//notAlone++;
+			$('button.green-hover[data-vid="'+id+'"]').disabled = true;
+			//notAloneEl.innerHTML = '(' + notAlone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
 		}
 	});
 }
