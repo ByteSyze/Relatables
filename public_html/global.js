40293f24baf00f128fd33168ccd93b66a90a6a11
Insomniac10102 <ins10102@gmail.com>
2015-04-06 23:13:12 -0700

    Adjust front end side of voting for new vote script

--- public_html/global.js
+++ public_html/global.js
@@ -393,35 +393,35 @@ function vote(id, vote, v)
 	.done(function(data) {
 		console.log(data);
 		
-		if(data == '00')
+		if(data == '2' && vote == 1)
 		{
 			//alone++;
 			//notAlone--;
 			
-			$('button.green-hover[data-vid="'+id+'"]').prop('disabled', false);;
-			$('button.red-hover[data-vid="'+id+'"]').prop('disabled', true);;
+			$('button.green-hover[data-vid="'+id+'"]').prop('disabled', false);
+			$('button.red-hover[data-vid="'+id+'"]').prop('disabled', true);
 			
 			//notAloneEl.innerHTML = '(' + notAlone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
 			//aloneEl.innerHTML = '(' + alone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
 		}
-		else if(data == '01')
+		else if(data == '2' && vote == 0)
 		{	
 			//notAlone++;
 			//alone--;
 			
-			$('button.green-hover[data-vid="'+id+'"]').prop('disabled', true);;
-			$('button.red-hover[data-vid="'+id+'"]').prop('disabled', false);;
+			$('button.green-hover[data-vid="'+id+'"]').prop('disabled', true);
+			$('button.red-hover[data-vid="'+id+'"]').prop('disabled', false);
 			
 			//notAloneEl.innerHTML = '(' + notAlone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
 			//aloneEl.innerHTML = '(' + alone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
 		}
-		else if(data == '10')
+		else if(data == '1' && vote == 1)
 		{
 			//alone++;
-			$('button.red-hover[data-vid="'+id+'"]').prop('disabled', true);;
+			$('button.red-hover[data-vid="'+id+'"]').prop('disabled', true);
 			//aloneEl.innerHTML = '(' + alone.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + ')';
 		}
-		else if(data == '11')
+		else if(data == '1' && vote == 0)
 		{
 			//notAlone++;
 			$('button.green-hover[data-vid="'+id+'"]').prop('disabled', true);;
