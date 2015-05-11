669e9f1e7e72f6ba2d36c22f894f94e735834339
Insomniac10102 <ins10102@gmail.com>
2015-05-11 16:50:46 -0700

    Update registration verification script
    Now with more popups!

--- public_html/verify/index.php
+++ public_html/verify/index.php
@@ -14,32 +14,27 @@
 	
 	$connection = GlobalUtils::getConnection();
 	
-	if($statement = $connection->prepare("SELECT verification FROM accounts WHERE id = (?)"))
+	$user = new User($id);
+	
+	if($user->getPending()) //Ensure user is pending
 	{
-		$statement->bind_param("i",$id);
-		
-		$statement->execute();
-		
-		$statement->store_result();
-		$statement->bind_result($db_verification);
-		$result = $statement->fetch();
-		
-		if(password_verify($verification, $db_verification))
+		if(password_verify($verification, $user->getVerification())) //Verify
 		{
-			if($statement = $connection->prepare("UPDATE accounts SET pending_email = NULL WHERE id = (?)"))
-			{
-				$statement->bind_param("i", $id);
-				
-				$statement->execute();
-				
-				$_SESSION['id'] = $id;
-				
-				mysqli_query($connection, "UPDATE accounts SET pending_email=NULL, verification=NULL WHERE id=".$id);
-				
-			}
+			$user->setPending(false);   // Verified! set user to no longer pending, give them a cookie, whatever.
+			$user->setVerification('');
+			
+			$user->update();
+			
 		}
-		
-		header("Location: /");
+		else
+		{
+			$_SESSION['error_msg'] = 'Invalid verification.';
+		}
 	}
-
-?>
+	else
+	{
+		$_SESSION['error_msg'] = 'Your account is already verified!';
+	}
+	
+	header("Location: /");
+	
