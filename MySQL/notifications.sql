168649a7be9032bd4b9857b3e2247ce761656c47
Insomniac10102 <ins10102@gmail.com>
2015-05-15 23:41:57 -0700

    Add separate href field to notifications table

--- MySQL/notifications.sql
+++ MySQL/notifications.sql
@@ -4,8 +4,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 14, 2015 at 10:13 PM
--- Server version: 5.1.61
+-- Generation Time: May 16, 2015 at 02:39 AM
+-- Server version: 5.1.73
 -- PHP Version: 5.2.17
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
@@ -31,12 +31,13 @@ CREATE TABLE IF NOT EXISTS `notifications` (
   `id` bigint(22) unsigned NOT NULL AUTO_INCREMENT,
   `uid` bigint(22) unsigned NOT NULL,
   `message` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  `href` varchar(128) COLLATE latin1_general_ci NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `seen` tinyint(1) NOT NULL DEFAULT '0',
   `deleted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `id` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
