b643b516ab1336f2c4601e722b702f5e65bbda81
Insomniac10102 <ins10102@gmail.com>
2015-05-29 14:49:43 -0700

    Update delete account popup

--- public_html/settings/settings.js
+++ public_html/settings/settings.js
@@ -2,16 +2,6 @@
 
 var descriptionCounter = document.getElementById('desc_char_counter');
 
-function hideDeletePopup()
-{
-	document.getElementById('delete-account-popup').style.display = 'none';
-}
-
-function showDeletePopup()
-{
-	document.getElementById('delete-account-popup').style.display = 'block';
-}
-
 function charCount(element,counter)
 {
 	counter.innerHTML = '(' + (130-element.value.length) + ' characters left)';
