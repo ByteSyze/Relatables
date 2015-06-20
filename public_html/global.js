23409c2a24d86d20383d9d99366f3cea8c9e3c74
Insomniac10102 <ins10102@gmail.com>
2015-06-20 15:39:57 -0700

    Add Google Analytics

--- public_html/global.js
+++ public_html/global.js
@@ -456,6 +456,15 @@ function vote(id, vote, v, unvote)
 	});
 }
 
+//Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-63194305-1', 'auto');
+ga('send', 'pageview');
+
 $('#pwrecoveryform').submit(function()
 {
 	$.post('/recover.php', {e: $(this).children().first().val(), s: 1}, function(data){ createPopup(data); });
