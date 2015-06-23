a93e9192026517294a9d7e2f9e9abce91bdb317b
Insomniac10102 <ins10102@gmail.com>
2015-06-22 19:33:51 -0700

    Initial adsense code

--- public_html/getposts.php
+++ public_html/getposts.php
@@ -13,3 +13,15 @@
 	
 	foreach($posts as $post)
 		$post->format();
+		
+	echo '
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- BannerUnit1 -->
+<ins class="adsbygoogle"
+	 style="display:block"
+	 data-ad-client="ca-pub-6838532745872751"
+	 data-ad-slot="8158747827"
+	 data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>';
