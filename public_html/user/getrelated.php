45c61d165a72321d1d30cf64ee1dacaccdc2e7bd
Insomniac10102 <ins10102@gmail.com>
2015-07-05 21:51:12 -0700

    Implement hide related functionality

--- public_html/user/getrelated.php
+++ public_html/user/getrelated.php
@@ -6,14 +6,28 @@
 	$index  = $_POST['i']; 
 	$uid	= $_POST['u'];
 	
-	$related = Post::getRelated($uid, $index, 3);
+	$connection = GlobalUtils::getConnection();
 	
-	for($i = 0; $i < 3; $i++)
-	{	
-		if($related[$i])
-			echo $related[$i]->format();
+	if($statement = $connection->prepare('SELECT flags FROM accounts WHERE uid=(?)'))
+	{
+		$statement->bind_param('i',$uid);
+		$statement->execute();
+		
+		$statement->bind_result($flags);
+		$statement->store_result();
+		
+		if(!(($flags >> 0x02) & 0x01)) // If not hiding related, get 'em
+		{
+			$related = Post::getRelated($uid, $index, 3);
+			
+			for($i = 0; $i < 3; $i++)
+			{	
+				if($related[$i])
+					echo $related[$i]->format();
+			}
+			
+			if($related[3]) //If a 6th post exists, that means there's atleast one more post to load.
+				echo '<span class="button" data-getrel="' . ($index+1) . '">Load More</span>';
+		}
 	}
 	
-	if($related[3]) //If a 6th post exists, that means there's atleast one more post to load.
-		echo '<span class="button" data-getrel="' . ($index+1) . '">Load More</span>';
-		
