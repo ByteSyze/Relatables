cc07c481833429dfe89b8321f7fcae7adb1dea35
Tyler <ins10102@gmail.com>
2016-03-12 07:10:23 -0800

    Add to .gitignore

--- public_html/global.php
+++ public_html/global.php
@@ -3,7 +3,7 @@
 	
 	ini_set("include_path", '/home/relatables/php:' . ini_get("include_path") );
 	
-	public $ROOTDIR = $_SERVER['DOCUMENT_ROOT'];
+	$ROOTDIR = $_SERVER['DOCUMENT_ROOT'];
 	
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/user.php';
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/post.php';
