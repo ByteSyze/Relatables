32fdf64588a86a1369f0c80fb33f4e0dec58089b
Insomniac10102 <ins10102@gmail.com>
2016-04-09 16:48:31 -0700

    Update navigation bar

--- public_html/navigation.php
+++ public_html/navigation.php
@@ -128,8 +128,41 @@ if($_SESSION['popup_msg'])
 	GlobalUtils::createPopupVisible("infopopup", "", "<span class='popup-small'>$popup</span>");
 }
 ?>
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <a class='navbar-brand' href='#'>
+	  <img alt="Relatables" src='/images/icons/logotextwhite.png'>
+	  </a>
+	</div>
 
-<div class="navigation-bar">
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	  <ul class="nav navbar-nav">
+		<li><a href="#">Moderate</a></li>
+		<li class="dropdown">
+		  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About <span class="caret"></span></a>
+		  <ul class="dropdown-menu">
+			<li><a href="#">FAQ</a></li>
+			<li><a href="#">Blog</a></li>
+		  </ul>
+		</li>
+	  </ul>
+	  <ul class="nav navbar-nav navbar-right">
+		<li><a href="#" data-toggle='modal' data-target='#myModal'>Log In</a></li>
+		<li><a href="#">Sign Up</a></li>
+	  </ul>
+	</div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<!--<div class="navigation-bar">
 	<div class="grid wrap wider no-gutters">
 		<div class="pull-left">
 			<ul class="navigation-items">
@@ -173,4 +206,4 @@ if($_SESSION['popup_msg'])
 		</div>
 	</div>
 </div>
-<div class="navigation-spacer"></div>
+<div class="navigation-spacer"></div>-->
