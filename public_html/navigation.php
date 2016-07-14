37a687b1265bf81c2f718b30ba861c0169938ea8
Insomniac10102 <ins10102@gmail.com>
2016-07-14 02:06:06 -0700

    Apply highlighting to correct navbar icon

--- public_html/navigation.php
+++ public_html/navigation.php
@@ -195,7 +195,7 @@ if(!empty($_SESSION['popup_msg']))
 		<li><a href="#" data-toggle='modal' data-target='#registerpopup'>Sign Up</a></li>
 		<?php else : ?>
 		<li class='dropdown'>
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><div class="icon glyphicon glyphicon-bell"></div></a>
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><div class="icon glyphicon glyphicon-bell <?php if($unreadNotifications) echo 'highlight'; ?> "></div></a>
 			<ul id="notif-drop"  class="dropdown-menu notifications-dropdown">
 				<?php
 					while($notification = mysqli_fetch_array($notifications)) :
@@ -221,7 +221,7 @@ if(!empty($_SESSION['popup_msg']))
 			</ul>
 		</li>
 		<li class='dropdown'>
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><div class="icon glyphicon glyphicon-user <?php if($unreadNotifications) echo 'highlight'; ?> "></div></a>
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><div class="icon glyphicon glyphicon-user"></div></a>
 			<ul id="prof-drop"  class="dropdown-menu">
 				<li><a href="/user/<?php echo GlobalUtils::$user->getUsername(); ?>">Profile</a></li><li><a href="/settings/account">Settings</a></li>
 				<li><a href="/signout.php">Signout</a></li>
